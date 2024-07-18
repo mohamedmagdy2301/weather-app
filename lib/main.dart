09d2f1f75a146b0884edec3d9f84f23a5bc51ca1
Mohamed magdy <mohammedmego15@gmail.com>
2024-07-18 05:28:04 +0300

    create ui today tap (not everything)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/screens/home_page.dart';
+import 'package:weather_app/screens/HomePage/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +13,12 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Weather App',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.grey,
+      //   scaffoldBackgroundColor: const Color.fromARGB(255, 52, 57, 72),
+      //   appBarTheme:
+      //       const AppBarTheme(backgroundColor: Color.fromARGB(255, 52, 57, 72)),
+      // ),
       home: HomePage(),
     );
   }
---
+++ lib/screens/HomePage/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/HomePage/taps/today_tap.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 123, 123, 123),
+        drawer: const Drawer(),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(197, 144, 144, 144),
+          centerTitle: true,
+          title: const Text(
+            'Monufia, Egypt',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+            ),
+          ),
+          actions: const [Icon(Icons.search)],
+          bottom: const TabBar(
+            isScrollable: true,
+            tabAlignment: TabAlignment.start,
+            labelColor: Color.fromARGB(255, 0, 32, 69),
+            indicatorColor: Color.fromARGB(255, 0, 32, 69),
+            unselectedLabelColor: Color.fromARGB(255, 255, 255, 255),
+            dividerHeight: 0,
+            tabs: [
+              Tab(text: 'Today'),
+              Tab(text: 'Forecast'),
+              Tab(text: 'Precipitation'),
+              Tab(text: 'Radar Sun & Moon'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            TodayTap(),
+            Center(child: Text('Forecast')),
+            Center(child: Text('Precipitation')),
+            Center(child: Text('Radar Sun & Moon')),
+          ],
+        ),
+      ),
+    );
+  }
+}
